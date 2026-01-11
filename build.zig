0e1622d8ec3c47313e0459ed6c7ae9abcdaf67c4
Jan200101 <sentrycraft123@gmail.com>
2026-01-11 22:45:40 +0100

    mark library as single threaded

--- build.zig
+++ build.zig
@@ -12,6 +12,7 @@ pub fn build(b: *std.Build) void {
             .target = target,
             .optimize = optimize,
             .link_libc = true,
+            .single_threaded = true,
         }),
     });
     b.installArtifact(lib);
